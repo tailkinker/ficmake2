f75932f5636281b7db6efe8dc6cdadaf21ea0d8b
Timothy Groves <the.tail.kinker@gmail.com>
2021-01-23 10:16:43 -0500

    Progress continues...

---
+++ fvolume.pas
@@ -0,0 +1,38 @@
+unit fvolume;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons;
+
+type
+
+  { TfrmVolume }
+
+  TfrmVolume = class(TForm)
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    BitBtn3: TBitBtn;
+    BitBtn4: TBitBtn;
+    BitBtn5: TBitBtn;
+    Button1: TButton;
+    Button2: TButton;
+    Label1: TLabel;
+    ListBox1: TListBox;
+  private
+
+  public
+
+  end;
+
+var
+  frmVolume: TfrmVolume;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
